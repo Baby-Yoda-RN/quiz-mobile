553465f41f858c3d79e9abcd1c13070e0de54635
Alphi Henry <aahenry51@gmail.com>
2022-03-17 12:53:50 -0400

    Changed the names of ListItem.Container and Listitem.style

--- components/ListItem/ListItem.js
+++ components/ListItem/ListItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {ListContainer} from './ListItem.Container';
-import {styles} from './ListItem.Styles';
+import {ListContainer} from './ListItem.view';
+import {styles} from './ListItem.styles';
 import {FlatList, Text, View} from 'react-native';
 
+//Use a map function to loop the array, setup the keys.
 const ListItem = () => {
   return <View style={styles.container}></View>;
 };
--- components/index.js
+++ components/index.js
@@ -1,6 +1,1 @@
 import React from 'react';
-import {FlatListBasics} from './ListItem/ListItem';
-
-//export const App = () => {
-//    return <FlatListBasics/>;
-//  };
