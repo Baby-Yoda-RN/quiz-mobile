6dd8e9999835f6a95ebebe71e8adbcb563e7c2e5
Dulaya <dulaya.saennok@gmail.com>
2022-03-17 14:06:58 -0700

    Header: remove an uncessary file

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { HeaderView } from './Header.view';
+import {View, Text} from 'react-native';
+import {style} from './Header.styles';
 
 export const Header = ({leftIcon = <></>, rightIcon = <></>, title, ...rest}) => {
- 
-    return <HeaderView 
-            leftIcon={leftIcon} // Any RN component
-            rightIcon={rightIcon} // Any RN component
-            title={title}  // Any RN component or string
-            {...rest}
-            />;
-}
+
+    return(
+        <View style={style.container} {...rest} >
+            <View>
+                {/*Conditional rendering leftIcon*/}
+                {leftIcon && leftIcon}
+            </View>
+            <View>
+                {/*Check if title exists. If so, check type and render conditionally.*/}
+                {title && (typeof title === 'string'? <Text>{title}</Text>:title )}
+            </View>
+            <View>
+                {rightIcon}
+            </View>
+        </View>
+    );
+};
+
