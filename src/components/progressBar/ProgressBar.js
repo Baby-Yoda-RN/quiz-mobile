ce7d4ee9c51819cf4548199e379d5ef995e708e7
Michael Raynes <raynesmike@yahoo.com>
2022-03-16 20:58:03 -0700

    progressbar: v.1

--- src/components/progressBar/ProgressBar.js
+++ src/components/progressBar/ProgressBar.js
@@ -1,4 +1,12 @@
 import React from 'react';
-export const ProgressBar = ({newValue, newMax}) => {
-  return <progress value={newValue} max={newMax} />;
+import {SafeAreaView, StatusBar, Text, View} from 'react-native';
+import {styles} from './progressBar.style';
+
+export const ProgressBar = ({value = '5%', max = '100'}) => {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.frontBar, {width: `${max}%`}]} />
+      <View style={[styles.backBar, {width: `${value}%`}]} />
+    </View>
+  );
 };
---
+++ src/components/progressBar/progressBar.style.js
@@ -0,0 +1,21 @@
+export const styles = {
+  container: {
+    flex: 1,
+    margin: 20,
+    justifyContent: 'center',
+  },
+  backBar: {
+    height: 15,
+    width: '5%',
+    backgroundColor: '#7199FF',
+    borderRadius: 50,
+    overflow: 'hidden',
+  },
+  frontBar: {
+    height: 15,
+    width: '100%',
+    backgroundColor: '#eeeee4',
+    borderRadius: 50,
+    position: 'absolute',
+  },
+};
