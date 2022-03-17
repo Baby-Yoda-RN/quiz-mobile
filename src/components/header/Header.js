9e80a3154d0ec48600051344c58f12bf153dc392
Dulaya <dulaya.saennok@gmail.com>
2022-03-17 00:50:37 -0700

    Header: add props to HeaderView

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,5 +3,9 @@ import { HeaderView } from './Header.view';
 
 export const Header = () => {
 
-    return <HeaderView />;
+    return <HeaderView 
+            leftIcon={<></>} // Any RN component
+            rightIcon={<></>} // Any RN component
+            title={'title'}  // Any RN component or string
+            />;
 }
