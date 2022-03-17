dec0f25af6450a788085da1e41b148622df44424
Miguel Luna <6733909-santiago.luna@users.noreply.gitlab.com>
2022-03-17 15:46:35 -0600

    renamed root folder and export component

---
+++ src/components/textInput/TextInput.styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+/*
+ * Styles used for TextInput component
+ */
+
+export const styles = StyleSheet.create({
+  textInputStyles: {
+    backgroundColor: '#fff', //White
+    borderColor: '#e5e5e5', //Gray
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10, //padding inside the TextView
+  },
+});
--- src/components/textInput/TextInput.view.js
+++ src/components/textInput/TextInput.view.js
@@ -2,12 +2,18 @@ import {TextInput as NativeTextInput, StyleSheet} from 'react-native';
 import React from 'react';
 import {styles} from './TextInput.styles';
 
+/*
+ * TextInput Component
+ * isSensitive is used to know if is a password text input
+ */
+
 export const TextInputView = ({
   placeHolder,
   isSensitive,
   customStyles,
   ...rest
 }) => {
+  //Merge default styles and user styles
   const flattenStyle = StyleSheet.flatten([
     styles.textInputStyles,
     customStyles,
