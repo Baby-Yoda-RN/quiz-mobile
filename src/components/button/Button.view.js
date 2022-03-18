f752995b62ac80bbd1aff02a1cdd9d95f661c6cd
carizzuti <charlierizzuti@gmail.com>
2022-03-18 16:25:49 -0400

    add missing imports

--- src/components/button/Button.view.js
+++ src/components/button/Button.view.js
@@ -1,4 +1,16 @@
 import React from 'react';
-import {Button} from '../button/Button';
+import { Text, TouchableOpacity, View} from 'react-native';
+import { styles } from './Button.styles';
 
-export function ButtonView({})
+export const ButtonView = ({title = 'Button', isDisabled = false, ...rest}) => {
+    let buttonStyle;
+    isDisabled ? buttonStyle = styles.buttonDisabled : buttonStyle = styles.buttonEnabled;
+    
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={[styles.buttonContainer, buttonStyle]} disabled={isDisabled} {...rest}>
+                <Text style={styles.title}>{title}</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
