0a4edc41454d15d08c2890b563abfffbc36a737d
leo-he-1 <tuh28889@temple.edu>
2022-03-18 15:45:07 -0400

    cleaned up code

--- src/components/container/Container.styles.js
+++ src/components/container/Container.styles.js
@@ -3,10 +3,8 @@ import {StyleSheet} from 'react-native';
 export const style = StyleSheet.create({
     containerDefault: {
         justifyContent: 'flex-start',
-        //alignItems: 'center',
     },
     containerCentered: {
         justifyContent: 'center',
-       // alignItems: 'center',
     },
 });
