78701370e5971fd7ae40e5a7005281b4f2345001
carizzuti <charlierizzuti@gmail.com>
2022-03-18 15:57:51 -0400

    reorganize code into view file

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import { TouchableOpacity, Text, View } from 'react-native';
-import { styles } from './Button.styles';
+import { ButtonView } from './Button.view';
 
 export const Button = ({title = 'Button', isDisabled = false, ...rest}) => {
-    let buttonStyle;
-    isDisabled ? buttonStyle = styles.buttonDisabled : buttonStyle = styles.buttonEnabled;
     
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity style={[styles.buttonContainer, buttonStyle]} disabled={isDisabled} {...rest}>
-                <Text style={styles.title}>{title}</Text>
-            </TouchableOpacity>
-        </View>
-
-    );
+    return <ButtonView title={title} isDisabled={isDisabled} />
 }
