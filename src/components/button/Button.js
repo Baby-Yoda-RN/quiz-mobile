19fb18030097aff5f273d480e8cb437155c260a2
carizzuti <charlierizzuti@gmail.com>
2022-03-18 15:46:32 -0400

    remove fixed dimension styles

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { TouchableOpacity, Text } from 'react-native';
+import { TouchableOpacity, Text, View } from 'react-native';
 import { styles } from './Button.styles';
 
 export const Button = ({title = 'Button', isDisabled = false, ...rest}) => {
     let buttonStyle;
-
-    if (isDisabled) {
-        buttonStyle = [styles.buttonEnabled, styles.buttonDisabled];
-    } else {
-        buttonStyle = styles.buttonEnabled;
-    }
+    isDisabled ? buttonStyle = styles.buttonDisabled : buttonStyle = styles.buttonEnabled;
     
     return (
-        <TouchableOpacity style={buttonStyle} disabled={isDisabled} {...rest}>
-            <Text style={styles.title}>{title}</Text>
-        </TouchableOpacity>
+        <View style={styles.container}>
+            <TouchableOpacity style={[styles.buttonContainer, buttonStyle]} disabled={isDisabled} {...rest}>
+                <Text style={styles.title}>{title}</Text>
+            </TouchableOpacity>
+        </View>
+
     );
 }
--- src/components/button/Button.styles.js
+++ src/components/button/Button.styles.js
@@ -1,20 +1,27 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-    buttonEnabled: {
-        backgroundColor: '#7199FF',
+    buttonContainer: {
         borderRadius: 8,
-        height: 60,
-        width: 319,
         justifyContent: 'center',
+        width: '75%',
+        height: '5%'
     },
     buttonDisabled: {
         backgroundColor: '#D1D1D1',
+    },
+    buttonEnabled: {
+        backgroundColor: '#7199FF',
     },
     title: {
         fontSize: 23,
         lineHeight: 34,
         textAlign: 'center',
         color: '#FFFFFF',
+    },
+    container: {
+        width: '100%',
+        height: '100%',
+        alignItems: 'center'
     }
 })
