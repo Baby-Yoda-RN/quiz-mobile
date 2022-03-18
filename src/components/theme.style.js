75a4b4982b4ce03eb3bc268f67fa86259da64187
Lasha Kvantaliani <lasharela@gmail.com>
2022-03-18 16:56:21 -0700

    refactor colors

--- src/components/theme.style.js
+++
@@ -1,8 +0,0 @@
-export const PRIMARY = '#7199FF';
-export const LIGHTGREY = '#E5E5E5';
-export const MIDGREY = '#D1D1D1';
-export const DARKGREY = '#C1C1C1';
-export const WHITE = '#FFFFFF';
-
-export const INITIAL_BAR_SIZE = 2;
-export const INITIAL_BAR_MARGIN = 2;
