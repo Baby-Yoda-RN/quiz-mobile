aee4a19e6c9caf7b6e78fa3ce25d062e91b9112c
leo-he-1 <tuh28889@temple.edu>
2022-03-19 12:25:24 -0400

    removed test code

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, SafeAreaView } from 'react-native';
 import {style} from './Container.styles';
 
-export const Container = ({children = <Text>hello</Text> , background , isVerticallyCentered = false, horizontalPadding = 32, ...rest}) =>{
+export const Container = ({children, background , isVerticallyCentered = false, horizontalPadding = 32, ...rest}) =>{
     let containerStyle;
     isVerticallyCentered ? containerStyle = style.containerCentered : containerStyle = style.container;
 
