06a07caaf61b885a8069b7373f535858ec09413b
Dulaya <dulaya.saennok@gmail.com>
2022-03-19 11:53:10 -0700

    Header: fix header

--- src/screens/dashboard/DashboardScreen.js
+++ src/screens/dashboard/DashboardScreen.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {Icon} from '../../components/icon/Icon'
+import {Icon} from '../../components/icon/Icon';
 import {Text, View} from 'react-native';
+import {Header} from '../../components';
 import {Button} from '../../components';
 import {ProgressBar} from '../../components/progressBar/ProgressBar';
 
 export const DashboardScreen = () => {
   return (
     <View>
-    <Icon iconSize={"regular"} iconName={"boxes"} iconColor={"#000000"}/>
-    <ProgressBar percentage={44} />
-    <Text>Initial Screen</Text>
-    <Button />
+      <Header />
+      <Icon iconSize={"regular"} iconName={"boxes"} iconColor={"#000000"}/>
+      <ProgressBar percentage={44} />
+      <Text>Initial Screen</Text>
+      <Button />
     </View>
   );
 };
