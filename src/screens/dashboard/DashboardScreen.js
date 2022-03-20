934a72914dc9875ecfdb43b01515c772b9c4c9a3
Michael Raynes <raynesmike@yahoo.com>
2022-03-20 16:58:25 -0700

    Miguels conflict resolved

--- src/screens/dashboard/DashboardScreen.js
+++ src/screens/dashboard/DashboardScreen.js
@@ -1,20 +1,47 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-import {Button, ProgressBar, Icon, Header} from '../../components';
+import {Text, View, FlatList} from 'react-native';
+import {Button, ProgressBar, Icon, Header, ListItem} from '../../components';
 
 export const DashboardScreen = () => {
+  const tests = [
+    {
+      title: 'Test 1',
+      subtitle: 'Test one is about this',
+    },
+    {
+      title: 'Test 2',
+      subtitle: 'Test two is about this',
+    },
+    {
+      title: 'Test 3',
+      subtitle: 'Test three is about this',
+    },
+    {
+      title: 'Test 4',
+      subtitle: 'Test four is about this',
+    },
+    {
+      title: 'Test 5',
+      subtitle: 'Test five is about this',
+    },
+    {
+      title: 'Test 6',
+      subtitle: 'Test six is about this',
+    },
+    {
+      title: 'Test 7',
+      subtitle: 'Test seven is about this',
+    },
+  ];
   return (
     <View>
       <Header />
-      <ProgressBar percentage={44} />
-      {/* Inline styles for example only */}
-
-      <Button
-        title={'Sample Button'}
-        onPress={() => console.log('Sample Button Press')}
-        isDisabled={false}
-        buttonStyle={{width: '75%', alignSelf: 'center', height: '20%'}}
-        titleStyle={{fontStyle: 'italic'}}
+      <FlatList
+        data={tests}
+        renderItem={({item}) => (
+          <ListItem title={item.title} subTitle={item.subtitle} />
+        )}
+        keyExtractor={item => item.id}
       />
     </View>
   );
