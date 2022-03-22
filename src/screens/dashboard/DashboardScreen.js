6215d396d70aecc69d5b21f493e0dbcc67f8e5e8
Nap1400 <napinelli@gmail.com>
2022-03-22 15:27:09 -0400

    Merge branch 'main' of https://github.com/Baby-Yoda-RN/quiz-mobile into nicholas/stepsProgress

--- src/screens/dashboard/DashboardScreen.js
+++ src/screens/dashboard/DashboardScreen.js
@@ -3,11 +3,13 @@ import {Icon} from '../../components/icon/Icon'
 import {Text, View} from 'react-native';
 import {Button} from '../../components';
 import {ProgressBar} from '../../components/progressBar/ProgressBar';
+import {StepsProggress} from '../../components/stepsProgress/StepsProgress';
 
 export const DashboardScreen = () => {
   return (
     <View>
     <Icon iconSize={"regular"} iconName={"boxes"} iconColor={"#000000"}/>
+    <StepsProggress currentStep={2} totalSteps={30}/>
     <ProgressBar percentage={44} />
     <Text>Initial Screen</Text>
     <Button />
