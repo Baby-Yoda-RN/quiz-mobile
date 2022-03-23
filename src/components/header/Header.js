031d7ae599aae978f2258bb0ef8888c28f56a94a
Michael Raynes <raynesmike@yahoo.com>
2022-03-22 17:33:14 -0700

    removed comment

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,10 +7,10 @@ import {size, color} from '../../theme';
 export const Header = () => {
   return (
     <HeaderView
-      // leftElement={
-      //   <Icon size={size.lg} iconName={'arrow-left'} color={color.primary} />
-      // }
-      //   headerTitle={<Text>3/20</Text>}
+      leftElement={
+        <Icon size={size.lg} iconName={'arrow-left'} color={color.primary} />
+      }
+        headerTitle={<Text>3/20</Text>}
       rightElement={
         <Icon size={size.lg} iconName={'user-circle'} color={color.primary} />
       }
