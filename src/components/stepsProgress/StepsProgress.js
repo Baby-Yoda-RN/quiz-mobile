927c9a4c8865307aec41d3e3ad1e891c63ba4d18
Nap1400 <napinelli@gmail.com>
2022-03-23 16:35:53 -0400

    Added stepsProgress to header

--- src/components/stepsProgress/StepsProgress.js
+++ src/components/stepsProgress/StepsProgress.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-export const StepsProggress = ({currentStep = 1, totalSteps}) => {
-    if (totalSteps === null || totalSteps === undefined){
+export const StepsProgress = ({
+    currentStep = 1, 
+    totalSteps = 20
+}) => {
+    if (!totalSteps || !Number.isInteger(totalSteps)){
         throw console.error('Value for totalSteps required');
     }
-
     return (
         <View>
             <Text>{currentStep}/{totalSteps}</Text>
