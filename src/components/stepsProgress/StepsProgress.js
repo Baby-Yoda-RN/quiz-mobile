e2b0774436d50c4b9aa151a5c6410ed302371685
Nap1400 <napinelli@gmail.com>
2022-03-23 16:43:44 -0400

    Added StepsProgress to Header

--- src/components/stepsProgress/StepsProgress.js
+++ src/components/stepsProgress/StepsProgress.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 export const StepsProggress = ({currentStep = 1, totalSteps}) => {
-    if (totalSteps === null || totalSteps === undefined){
+    if(!totalSteps || !Number.isInteger(totalSteps)){
         throw console.error('Value for totalSteps required');
     }
 
