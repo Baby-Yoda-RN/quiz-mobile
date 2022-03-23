925a14b21052ed49df18ead6ad4e10d07c50f531
Alphi Henry <aahenry51@gmail.com>
2022-03-23 18:19:43 -0400

    Intergrated Alphi's code; Changed shadow properties; Changed Border radius; Added new color 'LightGray2'

--- src/theme/color.js
+++ src/theme/color.js
@@ -1,8 +1,9 @@
 export const color = {
   white: '#ffffff',
   lightGray: '#F9F9F9',
+  lightGray2: '#CECECE',
   midGray: '#E5E5E5',
   darkGray: '#D1D1D1',
-  darkGreen:'#1E4D58', 
+  darkGreen: '#1E4D58',
   primary: '#7199FF',
 };
