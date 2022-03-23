f1fa1560b1d5dd7a0e64af548c7ec4c3deade337
leo-he-1 <tuh28889@temple.edu>
2022-03-23 19:22:05 -0400

    cleaned file

--- src/components/listView/ListView.js
+++ src/components/listView/ListView.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {ListViewInfo} from './ListView.view';
 import Axios from 'axios';
 
@@ -6,22 +6,15 @@ export const ListView = () => {
 
   const url = 'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/getalltests';
   const body = {TableName: 'Tests'};
-  const [data, setData] = useState("stuff");
+  const [tests, setData] = useState();
 
-  const getData =() =>{
-  Axios.post (url, body, {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  }).then (({data}) => {
-    setData(data.Items)
-    
-  }).catch(function(error){
-    console.log('Error');d
-  })
-}
-  getData();
-  console.log(data);
-  return <ListViewInfo data={data} />;
+  useEffect( () => {
+     Axios.post(url, body).then (({data}) => {
+      const returnData = data.Items;
+      const sortData = returnData.sort((firstItem, secondItem) => firstItem.id - secondItem.id);
+      setData(sortData);
+    }).catch(error => console.error(error))
+  }, []);
+  
+  return <ListViewInfo data={tests} />;
 };
