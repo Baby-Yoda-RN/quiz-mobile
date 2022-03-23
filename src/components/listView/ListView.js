db42ee87163185d4c87a6646a5a8b9c9c0bb855d
leo-he-1 <tuh28889@temple.edu>
2022-03-23 17:15:37 -0400

    cleaned up code

--- src/components/listView/ListView.js
+++ src/components/listView/ListView.js
@@ -1,15 +1,14 @@
 import React, {useState} from 'react';
 import {ListViewInfo} from './ListView.view';
 import Axios from 'axios';
-import {FlatList} from 'react-native';
 
 export const ListView = () => {
 
   const url = 'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/getalltests';
   const body = {TableName: 'Tests'};
-  const [data, setData] = useState("");
+  const [data, setData] = useState("stuff");
 
-  
+  const getData =() =>{
   Axios.post (url, body, {
     headers: {
       Accept: 'application/json',
@@ -21,7 +20,8 @@ export const ListView = () => {
   }).catch(function(error){
     console.log('Error');d
   })
- 
+}
+  getData();
   console.log(data);
   return <ListViewInfo data={data} />;
 };
