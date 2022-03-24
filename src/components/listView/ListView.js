113a3f55c1bcbed23e871cb1f052cd695fd4636d
leo-he-1 <tuh28889@temple.edu>
2022-03-24 15:11:39 -0400

    passed loading indicator to container

--- src/components/listView/ListView.js
+++ src/components/listView/ListView.js
@@ -1,19 +1,24 @@
 import React, {useState, useEffect} from 'react';
 import {ListViewInfo} from './ListView.view';
 import Axios from 'axios';
+import {Container} from '../container/Container';
 
 export const ListView = () => {
 
   const url = 'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/getalltests';
   const [tests, setData] = useState();
+  const [loading, setIsLoading] = useState(false);
 
   useEffect( () => {
+    setIsLoading(true);
      Axios.get(url).then (({data}) => {
       const returnData = data.Items;
       const sortData = returnData.sort((firstItem, secondItem) => firstItem.id - secondItem.id);
       setData(sortData);
+      <Container isLoading = {loading} />
+      setIsLoading(false);
     }).catch(error => console.error(error))
   }, []);
 
-  return <ListViewInfo data={tests} />;
+  return <Container isLoading = {loading} />, <ListViewInfo data={tests} />;
 };
