422fc68ee2fb29923a7341b015e90d88186610a5
leo-he-1 <tuh28889@temple.edu>
2022-03-23 22:52:05 -0400

    changed axios command to get

--- src/components/listView/ListView.js
+++ src/components/listView/ListView.js
@@ -5,16 +5,15 @@ import Axios from 'axios';
 export const ListView = () => {
 
   const url = 'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/getalltests';
-  const body = {TableName: 'Tests'};
   const [tests, setData] = useState();
 
   useEffect( () => {
-     Axios.post(url, body).then (({data}) => {
+     Axios.get(url).then (({data}) => {
       const returnData = data.Items;
       const sortData = returnData.sort((firstItem, secondItem) => firstItem.id - secondItem.id);
       setData(sortData);
     }).catch(error => console.error(error))
   }, []);
-  
+
   return <ListViewInfo data={tests} />;
 };
