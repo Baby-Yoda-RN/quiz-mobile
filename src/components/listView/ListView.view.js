0e3148d68dab4ff50fa9c20ecce84b5d0c5eed86
JoseF <jose.tf2396@gmail.com>
2022-03-24 16:33:37 -0700

    Removed unneeded comment

--- src/components/listView/ListView.view.js
+++ src/components/listView/ListView.view.js
@@ -8,10 +8,9 @@ export const ListViewInfo = ({data, ...rest}) => {
   return (
     <View style={styles.container}>
       <FlatList
-        //R
         contentContainerStyle={{padding: size.rg, paddingHorizontal: size.rg}}
         ItemSeparatorComponent={() => <View style={{padding: size.sm}} />}
-        data={data}
+        data={data}//
         renderItem={({item}) => (
           <ListItem title={item.title} subTitle={item.subtitle} />
         )}
