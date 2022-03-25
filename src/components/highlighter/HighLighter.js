7f4dba69330802df7a968bf267732095500baa72
Michael Raynes <raynesmike@yahoo.com>
2022-03-24 20:59:06 -0700

    Blocked with styles

--- src/components/highlighter/HighLighter.js
+++ src/components/highlighter/HighLighter.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Text} from 'react-native';
 import SyntaxHighlighter from 'react-native-syntax-highlighter';
-import reactSyntaxHighlighter from 'react-syntax-highlighter';
-import {docco} from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+// import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+// import {docco} from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 // import {docco} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 // import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 // import jsx from 'react-native-syntax-highlighter/dist/esm/styles/prism/jsx';
 import {size, color} from '../../theme';
+// import {prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
 
-export const Highlighter = (language = 'css', newCodeString) => {
+export const Highlighter = (language = 'css', newCodeString, ...rest) => {
   const oldCode = `
   position: absolute; 
   width: 319px;
@@ -20,16 +21,18 @@ export const Highlighter = (language = 'css', newCodeString) => {
   box-shadow: 0px 4px 4px
   rgba(206,206,206,0.16);  
   border-radius: 8px`;
-  // console.log(Prism);
+  // console.log(docco);
 
   const codeString = newCodeString && oldCode;
   return (
     // <Text>Hello</Text>
-    <reactSyntaxHighlighter
+    <SyntaxHighlighter
       language="css"
+      styles="dark"
       fontSize={size.rg + size.xs}
-      highlighter={docco}>
+      highlighter={'prism'}
+      {...rest}>
       {codeString}
-    </reactSyntaxHighlighter>
+    </SyntaxHighlighter>
   );
 };
