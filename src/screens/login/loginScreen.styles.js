04594871f0d922169b80455330d909dcfe41cf16
Miguel Luna <mlunav99@gmail.com>
2022-03-25 14:09:10 -0600

    Login Screen

---
+++ src/screens/login/loginScreen.styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native'
+import { size } from '../../theme'
+
+export const styles = StyleSheet.create({
+    textInput:{
+        marginBottom:size.rg,
+    },
+    button:{
+        marginVertical:size.rg,
+        paddingVertical:size.rg
+    },
+    container:{
+        justifyContent:'center'
+    }
+
+})
