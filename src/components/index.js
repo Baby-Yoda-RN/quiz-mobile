f73eac0ae1b109a33a36c594baec8cbd0dc8ab28
leo-he-1 <tuh28889@temple.edu>
2022-03-26 11:39:33 -0400

    moved listView logic into Dashboard

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 // export * from './textInput/TextInput';
 export * from './container/Container';
-export * from './listView/ListView';
 export * from './button/Button';
 export * from './icon/Icon';
 export * from './listItem/ListItem';
--- src/screens/dashboard/DashboardScreen.js
+++ src/screens/dashboard/DashboardScreen.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View} from 'react-native';
-import {Header, ListView} from '../../components';
+import Axios from 'axios';
+import {Header, Container} from '../../components';
+import { ListViewInfo } from '../../components/listView/ListView.view';
 
 export const DashboardScreen = () => {
+
+  const url = 'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/getalltests';
+  const [tests, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  
+  useEffect( () => {
+    const fetchData = async () =>{
+      setIsLoading(true);
+      try {
+        Axios.get(url).then (({data}) => {
+          const sortData = data.sort((firstItem, secondItem) => firstItem.id - secondItem.id);
+          setData(sortData);
+          setIsLoading(false);
+        })
+      }catch (error){
+        console.error(error)
+      }finally{
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+
   return (
     <View>
       <Header />
-      <ListView />
+      <ListViewInfo data={tests} />
+      <Container isLoading = {isLoading} />
     </View>
   );
 };
