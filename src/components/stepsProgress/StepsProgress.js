f5ee2775a79d385a61f7860751cd092b8c311c5a
Nap1400 <napinelli@gmail.com>
2022-03-26 11:30:50 -0400

    Added error message if currentStep is not integer.

--- src/components/stepsProgress/StepsProgress.js
+++ src/components/stepsProgress/StepsProgress.js
@@ -2,8 +2,13 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 export const StepsProgress = ({currentStep = 1, totalSteps = 20}) => {
+
   if (!totalSteps || !Number.isInteger(totalSteps)) {
     throw console.error('Value for totalSteps required');
+  }
+
+  if (!currentStep || !Number.isInteger(currentStep)){
+    throw console.error('Value for currentStep required');
   }
 
   return (
