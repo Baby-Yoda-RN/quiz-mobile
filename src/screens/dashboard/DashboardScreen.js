8585ba816af07a30d053816240a9030af86ad3c4
leo-he-1 <tuh28889@temple.edu>
2022-03-26 12:20:11 -0400

    added a config file for axios

--- src/screens/dashboard/DashboardScreen.js
+++ src/screens/dashboard/DashboardScreen.js
@@ -1,21 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import {View, FlatList, Text} from 'react-native';
-import Axios from 'axios';
+import {Text} from 'react-native';
 import {Header, Container,} from '../../components';
 import { ListViewInfo } from '../../components/listView/ListView.view';
-import {size} from '../../theme/size';
+import {quizAPI} from '../../configuration/Axios.configuration';
 
 export const DashboardScreen = () => {
 
-  const url = 'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/getalltests';
-  const [tests, setData] = useState();
+  //const url = 'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/getalltests';
+  const [quiz, setData] = useState();
   const [isLoading, setIsLoading] = useState(false);
   
   useEffect( () => {
     const fetchData = async () =>{
       setIsLoading(true);
       try {
-        Axios.get(url).then (({data}) => {
+        quizAPI.get().then (({data}) => {
           const sortData = data.sort((firstItem, secondItem) => firstItem.id - secondItem.id);
           setData(sortData);
           setIsLoading(false);
@@ -32,7 +31,7 @@ export const DashboardScreen = () => {
     <>
       <Header leftElement={<Text>Hello</Text>} />
       <Container isLoading={isLoading}>
-      <ListViewInfo data={tests} />
+      <ListViewInfo data={quiz} />
       </Container>
     </>
   );
