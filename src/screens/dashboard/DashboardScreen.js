b9824f31328145c068e10e90449c4759b839f5a9
leo-he-1 <70768391+leo-he-1@users.noreply.github.com>
2022-03-26 12:21:46 -0400

    Update DashboardScreen.js

--- src/screens/dashboard/DashboardScreen.js
+++ src/screens/dashboard/DashboardScreen.js
@@ -6,7 +6,6 @@ import {quizAPI} from '../../configuration/Axios.configuration';
 
 export const DashboardScreen = () => {
 
-  //const url = 'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/getalltests';
   const [quiz, setData] = useState();
   const [isLoading, setIsLoading] = useState(false);
   
