f9e85f85008e82315b72d67ec9ce0cea4afbeb4f
Nap1400 <napinelli@gmail.com>
2022-03-26 11:24:28 -0400

    This might be why it's not on main

--- src/components/stepsProgress/StepsProgress.js
+++ src/components/stepsProgress/StepsProgress.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-<<<<<<< HEAD
-export const StepsProgress = ({
-    currentStep = 1, 
-    totalSteps = 20
-}) => {
-    if (!totalSteps || !Number.isInteger(totalSteps)){
-        throw console.error('Value for totalSteps required');
-    }
-    return (
-        <View>
-            <Text>{currentStep}/{totalSteps}</Text>
-        </View>
-    );
-}
-=======
 export const StepsProgress = ({currentStep = 1, totalSteps = 20}) => {
   if (!totalSteps || !Number.isInteger(totalSteps)) {
     throw console.error('Value for totalSteps required');
@@ -29,4 +14,3 @@ export const StepsProgress = ({currentStep = 1, totalSteps = 20}) => {
     </View>
   );
 };
->>>>>>> d9e6de69e0d8e16b6e79127d258d1bd74cb36ede
