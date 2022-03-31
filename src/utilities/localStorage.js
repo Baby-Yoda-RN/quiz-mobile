764a4aba2f90d269c9f9fd03fffb52e4c8cc2cf4
Miguel Luna <mlunav99@gmail.com>
2022-03-30 20:34:03 -0600

    Async Storage Library and utilities file

---
+++ src/utilities/localStorage.js
@@ -0,0 +1,26 @@
+import {AsyncStorage} from '@react-native-async-storage/async-storage';
+
+export const getData = async key => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    if (typeof value === 'object') {
+      return JSON.parse(value);
+    }
+    return value;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const storeData = async (key, value) => {
+  try {
+    if (typeof value === 'object') {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem(key, jsonValue);
+    } else {
+      await AsyncStorage.setItem(key, value);
+    }
+  } catch (error) {
+    return error;
+  }
+};
