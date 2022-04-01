4a5ad50035c4c1987bff36a8d1e5b90eb85c1e98
Nicholas A Pinelli <napinelli@gmail.com>
2022-03-31 20:27:07 -0400

    Changed error message based on lasha's comments

--- src/components/stepsProgress/StepsProgress.js
+++ src/components/stepsProgress/StepsProgress.js
@@ -3,11 +3,11 @@ import {View, Text} from 'react-native';
 
 export const StepsProgress = ({currentStep = 1, totalSteps = 20}) => {
     if(!totalSteps || !Number.isInteger(totalSteps)){
-        throw console.error('Value for totalSteps required');
+        console.error('Value for totalSteps required');
     }
 
     if (!currentStep || !Number.isInteger(currentStep)){
-      throw console.error('Value for currentStep required');
+      console.error('Value for currentStep required');
     }
 
     return (
