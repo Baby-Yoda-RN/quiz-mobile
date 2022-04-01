4c06e6037bdb8947e21420addb65a978ec3334ca
leo-he-1 <tuh28889@temple.edu>
2022-04-01 13:55:34 -0400

    added some unfinished code to send the authorization token to server

--- src/configuration/Axios.configuration.js
+++ src/configuration/Axios.configuration.js
@@ -1,6 +1,17 @@
 import Axios from 'axios';
 
+let token;
+
+if(!token){
+  (async () => {
+    //token = LocalStorage.getData('AUTH_TOKEN');
+  })();
+  token = 'AUTH_TOKEN';
+}
+
 export const quizAPI = Axios.create({
   baseURL:
     'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/',
 });
+
+quizAPI.defaults.headers.common['Authorization'] = token;
