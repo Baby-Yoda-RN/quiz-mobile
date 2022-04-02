0d64fc14d3c77bac5333da38d9c4bc75dcb9b639
leo-he-1 <tuh28889@temple.edu>
2022-04-02 12:17:04 -0400

    fixed, now can assign token to getData

--- src/configuration/Axios.configuration.js
+++ src/configuration/Axios.configuration.js
@@ -1,19 +1,22 @@
 import Axios from 'axios';
+import {getData, storeData} from '../utilities/localStorage';
 
 let token;
 
 if(!token){
-  /*
   (async () => {
-    token = LocalStorage.getData('AUTH_TOKEN');
+    try{
+    token = await getData('AUTH_TOKEN');
+    }catch(error){
+      console.log(error);
+    }
+
   })();
-  */
-  token = 'AUTH_TOKEN';
 }
+
 
 export const quizAPI = Axios.create({
   baseURL:
     'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/',
 });
-
 quizAPI.defaults.headers.common['Authorization'] = token;
