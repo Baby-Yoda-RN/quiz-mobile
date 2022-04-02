8e6f8f920a5c54bf23e084491fbee2a86624393a
leo-he-1 <tuh28889@temple.edu>
2022-04-02 13:10:09 -0400

    modified axios conifg

--- src/configuration/Axios.configuration.js
+++ src/configuration/Axios.configuration.js
@@ -18,5 +18,7 @@ if(!token){
 export const quizAPI = Axios.create({
   baseURL:
     'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/',
+  headers: { 
+    Authorization: token
+  },
 });
-quizAPI.defaults.headers.common['Authorization'] = token;
