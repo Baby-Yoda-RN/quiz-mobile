af34d0e9866b9777b2280a9259226eaf831939f7
Nicholas A Pinelli <napinelli@gmail.com>
2022-04-04 19:40:16 -0400

    Profile navigation updates

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,74 +1,58 @@
-import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import React, {isValidElement} from 'react';
+import {View, SafeAreaView, Text, TouchableOpacity} from 'react-native';
 import {style} from './Header.style';
+import {Icon} from '../icon';
+import {isString, isEmpty} from 'lodash';
 
 export const Header = ({
-  leftElement,
-  rightElement,
+  leftIconName,
+  leftOnPress,
+  leftContainerStyle,
+
+  rightIconName,
+  rightContainerStyle,
+  rightOnPress,
+
   headerTitle,
-  leftElementContainerStyle,
-  rightElementContainerStyle,
+  headerElement,
   headerTitleContainerStyle,
   safeAreaViewStyle,
   ...rest
 }) => {
+  let headerContent;
+
+  if (headerElement && isValidElement(headerElement)) {
+    headerContent = headerElement;
+  } else if (isString(headerTitle) && !isEmpty(headerTitle)) {
+    headerContent = (
+      <View style={headerTitleContainerStyle}>
+        <Text>{headerTitle}</Text>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView style={safeAreaViewStyle}>
       <View style={style.container} {...rest}>
-        {<View>{!!leftElement} </View> &&
-          (React.isValidElement(leftElement) ? (
-            <View
-              style={[
-                style.leftElementContainerStyle,
-                leftElementContainerStyle,
-              ]}>
-              {leftElement}
-            </View>
-          ) : (
-            <Text
-              style={[
-                style.leftElementContainerStyle,
-                leftElementContainerStyle,
-              ]}>
-              {leftElement}
-            </Text>
-          ))}
-        {<View>{!!headerTitle}</View> &&
-          (React.isValidElement(headerTitle) ? (
-            <View
-              style={[
-                style.headerTitleContainerStyle,
-                headerTitleContainerStyle,
-              ]}>
-              {headerTitle}
-            </View>
-          ) : (
-            <Text
-              style={[
-                style.headerTitleContainerStyle,
-                headerTitleContainerStyle,
-              ]}>
-              {headerTitle}
-            </Text>
-          ))}
-        {<View>{!!rightElement}</View> &&
-          (React.isValidElement(rightElement) ? (
-            <View
-              style={[
-                style.rightElementContainerStyle,
-                rightElementContainerStyle,
-              ]}>
-              {rightElement}
-            </View>
-          ) : (
-            <Text
-              style={[
-                style.rightElementContainerStyle,
-                rightElementContainerStyle,
-              ]}>
-              {rightElement}
-            </Text>
-          ))}
+        {leftIconName && isValidElement(leftIconName) && (
+          <TouchableOpacity onPress={leftOnPress}>
+            <Icon
+              iconName={leftIconName}
+              customIconContainerStyle={leftContainerStyle}
+            />
+          </TouchableOpacity>
+        )}
+
+        {headerContent}
+
+        {rightIconName && isValidElement(rightIconName) && (
+          <TouchableOpacity onPress={rightOnPress}>
+            <Icon
+              iconName={rightIconName}
+              customIconContainerStyle={rightContainerStyle}
+            />
+          </TouchableOpacity>
+        )}
       </View>
     </SafeAreaView>
   );
