f4cc0236b6e070d5ef125e0b5bc2271ec81b1d66
Alphi Henry <aahenry51@gmail.com>
2022-04-04 21:35:39 -0400

    Eliminated the header

--- src/navigation/appNavigation/AppNavigation.js
+++ src/navigation/appNavigation/AppNavigation.js
@@ -1,20 +1,19 @@
-import React from "react";
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 //screens
-import {DashboardScreen} from '../../screens/dashboard/DashboardScreen'
-import {DetailScreen} from '../../screens/detail/DetailScreen'
-import {ResultScreen} from '../../screens/result/ResultScreen'
+import {DashboardScreen} from '../../screens/dashboard/DashboardScreen';
+import {DetailScreen} from '../../screens/detail/DetailScreen';
+import {ResultScreen} from '../../screens/result/ResultScreen';
 
-const AppStack = createNativeStackNavigator()
+const AppStack = createNativeStackNavigator();
 
 export const AppNavigation = () => {
-    
-    return(
-        <AppStack.Navigator>
-            <AppStack.Screen name="Dashboard" component={DashboardScreen}/>
-            <AppStack.Screen name="Detail" component={DetailScreen}/>
-            <AppStack.Screen name="Result" component={ResultScreen}/>
-        </AppStack.Navigator>
-    )
-}
+  return (
+    <AppStack.Navigator screenOptions={{headerShown: false}}>
+      <AppStack.Screen name="Dashboard" component={DashboardScreen} />
+      <AppStack.Screen name="Detail" component={DetailScreen} />
+      <AppStack.Screen name="Result" component={ResultScreen} />
+    </AppStack.Navigator>
+  );
+};
