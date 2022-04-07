5261a4bd6aa5c299e92cb615a6ac47ac091c8971
Dulaya <dulaya.saennok@gmail.com>
2022-04-07 01:20:49 -0700

    DetailScreen Refactor: separate logic from view

---
+++ src/screens/detail/DetailScreen.styles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+import { size} from '../../theme';
+
+export const styles = StyleSheet.create({
+  progressBar: {
+    marginVertical: size.rg,
+  },
+  textInput: {
+    marginTop: size.lg,
+  },
+});
