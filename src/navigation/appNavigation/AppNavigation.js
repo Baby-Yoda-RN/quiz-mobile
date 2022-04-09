5ef5ffd32254c30e8955eca6d79764b8742078c3
Alphi Henry <aahenry51@gmail.com>
2022-04-09 08:30:38 -0400

    Fixed conflicts

--- src/navigation/appNavigation/AppNavigation.js
+++ src/navigation/appNavigation/AppNavigation.js
@@ -2,19 +2,20 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 //screens
-import {DashboardScreen} from '../../screens/dashboard/DashboardScreen';
-import {DetailScreen} from '../../screens/detail/DetailScreen';
-import {ResultScreen} from '../../screens/result/ResultScreen';
+import {DashboardScreen} from '../../screens';
+import {DetailScreen} from '../../screens';
+import {ResultScreen} from '../../screens';
+import {ProfileScreen} from '../../screens';
 
 const AppStack = createNativeStackNavigator();
 
 export const AppNavigation = () => {
   return (
-    //Hello
-    <AppStack.Navigator>
+    <AppStack.Navigator screenOptions={{headerShown: false}}>
       <AppStack.Screen name="Dashboard" component={DashboardScreen} />
       <AppStack.Screen name="Detail" component={DetailScreen} />
       <AppStack.Screen name="Result" component={ResultScreen} />
+      <AppStack.Screen name="Profile" component={ProfileScreen} />
     </AppStack.Navigator>
   );
 };
