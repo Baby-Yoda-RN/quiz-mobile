a909eddd9c9a82663d42c1aabd25b7d8c73851e2
Michael Raynes <raynesmike@yahoo.com>
2022-04-10 20:28:44 -0700

    react-native-config added

--- android/app/src/main/java/com/dev_quiz_mobile/MainApplication.java
+++ android/app/src/main/java/com/dev_quiz_mobile/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -23,9 +24,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
+          
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          new ReactNativeConfigPackage();
           return packages;
         }
 
--- src/configuration/Axios.configuration.js
+++ src/configuration/Axios.configuration.js
@@ -1,10 +1,10 @@
 import Axios from 'axios';
 import {getData} from '../utilities/localStorage';
-import {TOKEN_KEY} from '../context/AppContext' 
+import {TOKEN_KEY} from '../context/AppContext';
+import Config from 'react-native-config';
 
 let quizAPI = Axios.create({
-  baseURL:
-    'http://ec2-54-218-161-100.us-west-2.compute.amazonaws.com:3000/api/',
+  baseURL: Config.API_URL,
 });
 
 (async () => {
