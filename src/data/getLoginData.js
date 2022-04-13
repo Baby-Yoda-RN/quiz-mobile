94621891040c2d39672b9bdce121ca96cd5b8dfd
Miguel Luna <mlunav99@gmail.com>
2022-04-12 19:59:31 -0500

    Catch Login Errors

--- src/data/getLoginData.js
+++ src/data/getLoginData.js
@@ -3,14 +3,15 @@ import {quizAPI} from '../../src/configuration/Axios.configuration';
 export const getLoginData = async (values) => {
   try {
     const response = await quizAPI.post('/login', {
-      Email: values.email,
-      Password: values.password,
+      email: values.email,
+      password: values.password,
     });
-    if (response.data === 'Wrong Email or Password.') {
-      return {token: null, error: response.data};
-    }
     return {token: response.data, error: false};
   } catch (error) {
-    console.log(error);
+    const response = await JSON.parse(JSON.stringify(error))
+    if(response.status === 401){
+      return {token: null, error: 'Wrong Email or Password'}
+    }
+      return {token: null, error: 'Something went wrong'}
   }
 };
